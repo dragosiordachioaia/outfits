1074b0feb5ca233542fe7f3195f00cae1ebeb636
dragos <xdragosy@gmail.com>
2017-09-15 15:37:26 +0100

    add comments to the clean.sh script

--- clean.sh
+++ clean.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# remove the cache of python dependencies
 rm -rf __pycache__
 
+# remove the front-end javascript dependencies
 rm -rf node_modules
 
+# deactivate the python virtual environment
 deactivate
 
+# delete the python virtual environment
 rm -rf venv
