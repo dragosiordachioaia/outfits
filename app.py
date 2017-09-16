640db43a69ade31976759b1a6334381efba09d42
dragos <xdragosy@gmail.com>
2017-09-16 09:23:21 +0100

    add basic caching

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template, request, jsonify
+from flask_caching import Cache
 from tinydb import TinyDB
 from flask_cors import CORS
 
 app = Flask(__name__)
 db = TinyDB('db.json')
 CORS(app)
+# Check Configuring Flask-Caching section for more details
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 @app.route('/')
+@cache.cached(timeout=3600)
 def home():
     return render_template('index.html')
 
 @app.route('/elements')
+# @cache.cached(timeout=10)
 def get_all_elements():
     elements = db.all()
     return jsonify(elements)
